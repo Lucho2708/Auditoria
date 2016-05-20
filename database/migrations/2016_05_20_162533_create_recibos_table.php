d2fd52f44320791c5f36290cc828ce67a04d2753
Luis Eduardo Silva Rodriguez <Luis.Silva.2708@gmail.com>
2016-05-20 11:28:15 -0500

    Tabla de Migracion Recibo

---
+++ database/migrations/2016_05_20_162533_create_recibos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecibosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recibos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cod');
+            $table->integer('fac');
+            $table->string('val');
+            $table->string('desc');
+            $table->integer('id_user');
+            $table->timestamps();
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recibos');
+    }
+}
+
+
