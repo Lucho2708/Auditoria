c4870575d015e2a0db355433809a5edfa87702b7
edgarlozano12 <edgarlozano93@gmail.com>
2016-05-26 17:42:42 -0500

    actualizacion de LOGSssss

---
+++ app/Http/Requests/EditReciboRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Auditoria\Http\Requests;
+
+use Auditoria\Http\Requests\Request;
+
+class EditReciboRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cod'=>'required|min:1|max:10',
+            'fac'=>'required|min:1|max:10',
+            'val'=>'required|min:1|max:10|regex:/^[0-9]{1,3}([\\,][0-9]{3})*[\\.][0-9]{2}$/',
+            'desc'=>'required|min:1|max:30'
+        ];
+    }
+}
